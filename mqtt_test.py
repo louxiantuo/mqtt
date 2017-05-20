43a9214eedd893054f9344104775e0392f07c777
louxiantuo <louxiantuo@outlook.com>
2017-05-20 17:03:31 +0800

    add

--- mqtt_test.py
+++ mqtt_test.py
@@ -7,7 +7,7 @@ def on_connect(client, userdata, flags, rc):
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe("sun")
-
+#Subscrib sun 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
